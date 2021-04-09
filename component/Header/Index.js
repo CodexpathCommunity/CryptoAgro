2be48c88ce09d1175ca8d69b5d5287198e88bd3d
Skiboybl <69096827+CodexpathCommunity@users.noreply.github.com>
2021-04-09 22:46:49 +0100

    started the cryptoAgro app

---
+++ component/Header/Index.js
@@ -0,0 +1,11 @@
+import { HeaderContainer } from "./HeaderElement";
+
+function Header() {
+  return (
+    <HeaderContainer>
+      <h1>this is the header seciton.</h1>
+    </HeaderContainer>
+  );
+}
+
+export default Header;
