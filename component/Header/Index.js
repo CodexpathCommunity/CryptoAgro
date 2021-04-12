c9f9df8ee43e2da7e1a28c36cd0de5c048dbd51b
Skiboybl <69096827+CodexpathCommunity@users.noreply.github.com>
2021-04-12 07:45:21 +0100

    a new login component

--- component/Header/Index.js
+++ component/Header/Index.js
@@ -13,42 +13,9 @@ import Modal from "@material-ui/core/Modal";
 import { useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, provider } from "../../firebase";
-
-function rand() {
-  return Math.round(Math.random() * 20) - 10;
-}
-
-function getModalStyle() {
-  const top = 50 + rand();
-  const left = 50 + rand();
-
-  return {
-    top: `${top}%`,
-    left: `${left}%`,
-    transform: `translate(-${top}%, -${left}%)`,
-  };
-}
-
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    position: "absolute",
-    width: 400,
-    backgroundColor: theme.palette.background.paper,
-    border: "2px solid #000",
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-    display: "flex",
-    alignItems: "center",
-    flexDirection: " column",
-  },
-}));
+import LoginModal from "./LoginModal";
 
 function Header() {
-  const classes = useStyles();
-  // getModalStyle is not a pure function, we roll the style only on the first render
-  const [modalStyle] = useState(getModalStyle);
-  const [open, setOpen] = useState(false);
-
   const handleOpen = () => {
     setOpen(true);
   };
@@ -62,12 +29,6 @@ function Header() {
     auth.signInWithPopup(provider).catch(alert);
   };
 
-  const body = (
-    <div style={modalStyle} className={classes.paper}>
-      <LoginHeader>Login to continue</LoginHeader>
-      <HeadeBtn onClick={signIn}>Continue With Google </HeadeBtn>
-    </div>
-  );
   const [user] = useAuthState(auth);
 
   return (
@@ -95,7 +56,7 @@ function Header() {
           aria-labelledby="simple-modal-title"
           aria-describedby="simple-modal-description"
         >
-          {body}
+          <LoginModal setOpen={setOpen} />
         </Modal>
       </HeaderContent>
     </HeaderContainer>
